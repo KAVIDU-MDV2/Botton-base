e90d69d500e08dcb390880d5c46d6f9a4a274a3d
DILSHAN23924 <dashinsa09@gmail.com>
2025-08-15 19:41:16 +0530

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+
+module.exports = {    
+SESSION_ID: process.env.SESSION_ID || 'CHAMA-MD=rUtHWCYL#ajNtDyO1EIQzqtef3mE6t-j00WnxH4ZV0ApI8_EFBcE',
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94772194789",
+ALIVE: process.env.ALIVE || `default`,
+OWNER_NAME: process.env.OWNER_NAME || 'Dilshan' ,     
+POSTGRESQL_URL: process.env.POSTGRESQL_URL || 'postgres://izumimd_meje_user:0Vhm5vKGZ7ORt2FlJBQf4d6EtRdeuE8z@dpg-cn0o2imn7f5s73fa46q0-a.frankfurt-postgres.render.com/izumimd_meje',
+PREFIX:  process.env.PREFIX || ['.'] ,
+FOOTER: '> ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴀᴅʜɪ ᴏꜰᴄ',
+DIRECTION: true,
+IMAGE: process.env.IMAGE || `https://i.ibb.co/B25Ws4nf/temp-image.jpg`,
+LOGO: process.env.LOGO || `https://i.ibb.co/B25Ws4nf/temp-image.jpg`     
+};
